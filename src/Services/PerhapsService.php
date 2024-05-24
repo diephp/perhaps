d4ff775fa12d06744c2eeda8ab970309e7c118a3
tosic <a.astapov@autodoc.eu>
2024-05-24 18:29:14 +0200

    init

---
+++ src/Services/PerhapsService.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace DiePHP\Perhaps\Services;
+
+use DiePHP\Perhaps\Exceptions\PerhapsException;
+use Psr\Log\LoggerInterface;
+use Traversable;
+
+class PerhapsService
+{
+
+    /**
+     * @var \Psr\Log\LoggerInterface|null
+     */
+    private ?LoggerInterface $logger            = null;
+
+    private array            $excludeExceptions = [];
+
+    public function __construct(
+        LoggerInterface $logger = null,
+        array           $excludeExceptions = []
+
+    )
+    {
+        $this->logger = $logger;
+        $this->excludeExceptions = $excludeExceptions;
+    }
+
+    /**
+     * Retries a given function a specified number of times with a delay between each retry.
+     * @param callable         $function      The function to retry.
+     * @param int              $trys          The number of times to retry the function. Default is 2.
+     * @param Traversable|null $delaySequence The sequence of delays between each retry. Default is null.
+     * @return mixed The result of the function call if successful.
+     * @throws \Exception When an exception occurs and is not excluded.
+     * @throws \Exception When all retries fail.
+     */
+    public function retry(callable $function, int $trys = 2, Traversable $delaySequence = null)
+    {
+        $delay = (int) $delaySequence ? $delaySequence->current() : 0;
+
+        for ($iter = 1; $iter <= $trys; $iter++) {
+            try {
+                return \call_user_func($function, $iter);
+            } catch (\Exception $exception) {
+                if (\in_array(\get_class($exception), $this->excludeExceptions)) {
+                    throw $exception;
+                }
+
+                if ($this->logger) {
+                    $this->logger->warning($exception->getMessage(), [
+                        'delay'    => $delay,
+                        'iter'     => $iter,
+                        'trys'     => $trys,
+                        'sequence' => $delaySequence ? get_class($delaySequence) : $delaySequence,
+                        'previous' => $exception->getPrevious(),
+                    ]);
+                }
+
+                \usleep($delay);
+
+                if ($delaySequence) {
+                    $delaySequence->next();
+                    $delay = $delaySequence->current();
+                }
+
+            }
+        }
+
+        if (isset($exception)) {
+            throw $exception;
+        }
+
+        throw new PerhapsException("Perhaps::retry have no success result");
+    }
+
+}
